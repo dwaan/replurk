04b3f7eff706d968c41049098ce24661f255f37c
Dwan <me@dwaan.com>
2021-03-29 17:27:06 +0300

    Ignoring more files

--- part_head.php
+++
@@ -1,1 +0,0 @@
-<div id="corner"><div class="tl"></div><div class="tr"></div><div class="br"></div><div class="bl"></div></div><div class="support"><div class="content"><h1>😭</h1><h2>Please don't squaze me 😱.</h2><p>I think your screen is just to small for me to support it.</p><p>Please rotate your 📱 screen or resize your 💻 browser for better looking layout.</p></div></div>
